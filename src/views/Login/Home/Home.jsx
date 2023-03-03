bb2f3a1763f29d9f66f02691db05b6ff0b839c87
julia-gegers <juliagegersla@gmail.com>
2023-03-03 15:40:44 -0300

    anterior

--- src/views/Login/Home/Home.jsx
+++ src/views/Login/Home/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Home = () => {
 return (
     <div>
-        <h1>Products</h1>
+        <h1>Home</h1>
         <button>
             <Link to={`/products/1`}>Produtos</Link>
         </button>
